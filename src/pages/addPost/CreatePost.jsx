a40b8212261c438b1a259a7d44e18a3950df8d26
Om <omdesh2003@gmail.com>
2024-02-10 01:17:01 +0530

    create post

--- src/pages/addPost/CreatePost.jsx
+++ src/pages/addPost/CreatePost.jsx
@@ -6,8 +6,10 @@ import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { useState } from 'react';
 import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const CreatePost = () => {
+  const [editorData, setEditorData] = useState('');
   const [files, setFiles] = useState(null);
   const { register, handleSubmit, formState: { errors } } = useForm({
     resolver: yupResolver(addPostSchema)
@@ -23,8 +25,7 @@ const CreatePost = () => {
     formData.append('title', data.title);
     formData.append('tags', data.tags);
     formData.append('image', files[0]); // Append the image file to the FormData
-  
-    console.log("formData", formData);
+    formData.append('body', editorData);
   
     try {
       const response = await axios.post('http://localhost:3000/post', formData, {
@@ -33,18 +34,20 @@ const CreatePost = () => {
         },
       });
       if (response.status === 200) {
-        console.log('Post created successfully');
+        toast.success('Post created successfully');
       }
     } catch (error) {
       console.error('Error creating post:', error);
     }
   };
   
+  const handleEditorChange = (event, editor) => {
+    const data = editor.getData();
+    setEditorData(data);
+  };
   
-
   return (
     <div className="w-full lg:w-1/2 mx-auto p-6 bg-white rounded-lg shadow-lg">
-      {/* <h2 className="text-2xl font-bold mb-4">Create a New Post</h2> */}
       <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
         <div className="flex md:flex-row flex-col gap-4">
           <div className='w-full'>
@@ -63,18 +66,15 @@ const CreatePost = () => {
           <input type="file" onChange={handleFileChange} className="w-full border border-gray-300 rounded p-2" />
           {errors.image && <p className="text-red-500 text-sm">{errors.image.message}</p>}
         </div>
-        {/* <div>
+        <div>
           <label htmlFor="body" className="block">Body:</label>
           <CKEditor
             editor={ClassicEditor}
-            data="<p>Tell your story!</p>"
-            onChange={(event, editor) => {
-              const data = editor.getData();
-              register('body').onChange(data);
-            }}
+            data={editorData} // Set initial data from state
+            onChange={handleEditorChange} // Handle editor changes
           />
           {errors.body && <p className="text-red-500 text-sm">{errors.body.message}</p>}
-        </div> */}
+        </div>
 
 
         <button type="submit" className="bg-gray-800 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300">Create Post</button>
--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -2,7 +2,7 @@ import * as Yup from "yup";
 
 export const addPostSchema = Yup.object().shape({
   title: Yup.string().required("Title is a required field").label("Title"),
-  //   body: Yup.string().required("Body is a required field").label("Body"),
+  // body: Yup.string().required("Body is a required field").label("Body"),
   tags: Yup.string().label("Tags"),
-//   image: Yup.mixed().required("Image is a required field").label("Image"),
+  // image: Yup.mixed().required("Image is a required field").label("Image"),
 });
